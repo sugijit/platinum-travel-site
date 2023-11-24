5ad867106b2966e6e0c91abbaba984ec95452ed2
sugijit <b.suldsaikhan@gmail.com>
2023-11-24 22:36:24 +0900

    modal overlay hiij duslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,8 +5,27 @@ import StickyHeader from "./modules/StickyHeader";
 
 let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
+
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
+
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkName 'modal'*/ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => console.log("aldaa garlaa"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
